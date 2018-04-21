7918db1d44e5524a103f45e67a832cb54b1bbd10
BlackDeft <jacekstachecki84@gmail.com>
2018-04-21 10:01:18 +0200

    Ukonczenie menu

--- menu.cpp
+++ menu.cpp
@@ -3,10 +3,15 @@
 using namespace std;
 
 extern bool koniec;
-void tutorial()
+void pojedynek();
+void start();
+void menu();
+
+
+void PvS()
 {
     int wybor;
-    cout<<"1.Tutorial"<<endl<<"2.Pojedynek"<<endl<<"3.wroc";
+    cout<<"1.Easy"<<endl<<"2.Normal"<<endl<<"3.Hard"<<endl<<"4.Wroc";
     cin>>wybor;
 switch( wybor)
 {
@@ -16,16 +21,22 @@ case 1:
 
 case 2:
     system("cls");
+    break;
+case 3:
+    system("cls");
+    break;
+case 4:
+    system("cls");
+    pojedynek();
     break;
 default:
     break;
 }
 }
-
 void pojedynek()
 {
     int wybor;
-    cout<<"1.Easy"<<endl<<"2.Normal"<<endl;
+    cout<<"1.Player vs Player"<<endl<<"2.Player vs SI"<<endl<<"3.Wroc"<<endl;
     cin>>wybor;
 switch( wybor)
 {
@@ -35,6 +46,10 @@ case 1:
 
 case 2:
     system("cls");
+    break;
+case 3:
+    system("cls");
+    start();
     break;
 default:
     break;
@@ -44,18 +59,21 @@ default:
 void start()
 {
     int wybor;
-    cout<<"1.Tutorial"<<endl<<"2.Pojedynek"<<endl;
+    cout<<"1.Tutorial"<<endl<<"2.Pojedynek"<<endl<<"3.Wroc"<<endl;
     cin>>wybor;
 switch( wybor)
 {
 case 1:
     system("cls");
-    tutorial();
     break;
 
 case 2:
     system("cls");
     pojedynek();
+    break;
+case 3:
+    system("cls");
+    menu();
     break;
 default:
     break;
