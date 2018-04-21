7f8760256c214745cde3d12b5c9ee51e34e9bad7
TheGreenWolff <AdamHeinrich1@wp.pl>
2018-04-21 21:14:43 +0200

    PvSIv1

--- rozgrywka.cpp
+++ rozgrywka.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include <cstdlib>
+#include <ctime>
+#include <time.h>
 
 
 using namespace std;
@@ -25,34 +27,97 @@ void strzal(int x,int y,int &statki)
 
 }
 
+void strzalSI(int xSI,int ySI,int &statkiSI)
+{
+    if(planPLS[xSI-1][ySI-65]=='V')
+    {
+        cout<<"Trafiony zatopiony!!"<<endl;
+        planSIs[xSI-1][ySI-65]='X';
+        statkiSI--;
+    }
+    else
+    {
+        planSIs[xSI-1][ySI-65]='O';
+        cout<<"Pudlo!!"<<endl;
+    }
+
+}
 
 void tutorial()
 {
 
-    int statki=10,x,strzaly=0;
+    int statki=15,x,strzaly=0;
     char y;
-    cout<<"informacje o poprzednim strzale:"<<"Brak"<<endl;
-    cout<<"Mapa strzalow"<<endl;
+    cout << "Informacje o poprzednim strzale: " << "Brak" << endl;
+    cout << "Mapa strzalow"<<endl;
     wypplan(planPLS);
-    cout<<"Podaj kordynacje strzalu (po kazdym parametrze w przedziale <1,10>  enter):"<<endl;
-    cin>>x;
-    cin>>y;
+    cout << "Podaj kordynacje strzalu (po kazdym parametrze w przedziale <1,duze A-J>  enter): " << endl;
+    cin >> x;
+    cin >> y;
     do
     {
         system("cls");
-        cout<<"informacje o poprzednim strzale:";
+        cout << "Informacje o poprzednim strzale: ";
         strzal(x,y,statki);
-        cout<<endl;
-        cout<<"Mapa strzalow"<<endl;
+        cout << endl;
+        cout << "Mapa strzalow" << endl;
         wypplan(planPLS);
-        cout<<"Podaj kordynacje strzalu (po kazdym parametrze enter):"<<endl;
-        cin>>x;
-        cin>>y;
+        cout << "Podaj kordynacje strzalu (po kazdym parametrze enter): " << endl;
+        cin >> x;
+        cin >> y;
 
         strzaly++;
     }
     while(statki!=0);
 
 }
+
+void PVSI()
+{
+    srand(time(NULL));
+     int statki=15,x,strzaly=0,statkiSI=15,xSI,strzalySI=0;
+    char y,ySI;
+    cout << "Informacje o poprzednim strzale: " << "Brak" << endl;
+    cout << "Mapa strzalow" << endl;
+    wypplan(planPLS);
+    cout << "Podaj kordynacje strzalu (po kazdym parametrze w przedziale <1,duze A-J>  enter): " << endl;
+    cin >> x;
+    cin >> y;
+    do
+    {
+        system("cls");
+        cout << "Informacje o poprzednim strzale: ";
+        strzal(x,y,statki);
+        cout << endl;
+        cout << "Mapa strzalow" << endl;
+        wypplan(planPLS);
+        cout << "Podaj kordynacje strzalu (po kazdym parametrze enter): " << endl;
+        cin >> x;
+        cin >> y;
+
+        strzaly++;
+    }
+    while(statki!=0);
+
+    cout << endl;
+    cout << "Mapa wroga" << endl;
+    wypplan(planSIs);
+    do
+    {
+        system("cls");
+        cout << "Informacje o wrogu: ";
+        strzalSI(xSI,ySI,statkiSI);
+        cout << endl;
+        cout << "Mapa wroga" << endl;
+        wypplan(planSIs);
+        cout << "Kordynacje podane przez komputer: "<< xSI << ySI << endl;
+        x=rand()%10;
+        y=rand()%10;
+
+        strzalySI++;
+    }
+    while(statkiSI!=0);
+}
+
 
 
